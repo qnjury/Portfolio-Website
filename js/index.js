438d98260aa9d105affb18a85e5cc437413b85be
qnjury <76183767+qnjury@users.noreply.github.com>
2020-12-18 09:12:36 +0300

    Added Initial portfolio files

---
+++ js/index.js
@@ -0,0 +1,10 @@
+// Navbar active on scroll function
+$(function () {
+    $(window).on('scroll', function () {
+        if ( $(window).scrollTop() > 10 ) {
+            $('.navbar').addClass('active');
+        } else {
+            $('.navbar').removeClass('active');
+        }
+    });
+});
